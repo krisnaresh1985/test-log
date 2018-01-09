3e317e8b521336ac8b36a7a12c5ced865247f828
Sundaram Krishnamoorthy <176912@cognizant.com>
2018-01-08 22:14:40 -0600

    second commit

--- src/main/java/com/parse/json/jsonpath/Jsonparse.java
+++ src/main/java/com/parse/json/jsonpath/Jsonparse.java
@@ -22,8 +22,8 @@ public class Jsonparse
 	
     public static void main( String[] args )
     {
-    	String json = "[       {       \"memberId\": \"A0002\",       \"subscriberId\": \"A0002\",       \"startDate\": \"01/12/2017\",       \"endDate\": \"12/15/2017\",       \"limit\": 10,       \"network\": \"INB\",       \"benefit\": \"Specialist Office Visit\",       \"accumulation\": 60,       \"units\": 5    },       {       \"memberId\": \"A0002\",       \"subscriberId\": \"A0002\",       \"startDate\": \"01/12/2017\",       \"endDate\": \"12/15/2017\",       \"limit\": 10,       \"network\": \"INB\",       \"benefit\": \"Specialist Office Visit\",       \"accumulation\": 60,       \"units\": 5    } ]";
-    	Jsonparse jsonparse = new Jsonparse(json);
+    	String json1 = "[       {       \"memberId\": \"A0002\",       \"subscriberId\": \"A0002\",       \"startDate\": \"01/12/2017\",       \"endDate\": \"12/15/2017\",       \"limit\": 10,       \"network\": \"INB\",       \"benefit\": \"Specialist Office Visit\",       \"accumulation\": 60,       \"units\": 5    },       {       \"memberId\": \"A0002\",       \"subscriberId\": \"A0002\",       \"startDate\": \"01/12/2017\",       \"endDate\": \"12/15/2017\",       \"limit\": 10,       \"network\": \"INB\",       \"benefit\": \"Specialist Office Visit\",       \"accumulation\": 60,       \"units\": 5    } ]";
+    	Jsonparse jsonparse = new Jsonparse(json1);
     	System.out.println(jsonparse.getValue("$..limit"));        
 //        Object document = Configuration.defaultConfiguration().jsonProvider().parse(json);
 //        System.out.println(JsonPath.read(document, "$.store.book[0].author"));
